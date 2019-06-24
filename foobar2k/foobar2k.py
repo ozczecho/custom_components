cd5503d2d79629e39298db13e73c4bf33223619d
Mike Barnert <ozczecho@yahoo.com>
2019-06-24 15:56:36 +1000

    initial check in for foobar 2000 player component

---
+++ foobar2k/foobar2k.py
@@ -0,0 +1,282 @@
+import requests
+import logging
+import json.tool
+import time
+
+_LOGGER = logging.getLogger("Foobar2k")
+
+# System const
+POWER_ON = "ON"
+POWER_OFF = "OFF"
+STATE_PAUSED = "PAUSED"
+STATE_STOPPED = "STOPPED"
+STATE_PLAYING = "PLAYING"
+
+# Api calls
+GET_PLAYER_INFO = "/api/player"
+
+POST_PLAYER = "/api/player"
+POST_PLAYER_PLAY = "/api/player/play"
+POST_PLAYER_STOP = "/api/player/stop"
+POST_PLAYER_NEXT = "/api/player/next"
+POST_PLAYER_PREVIOUS = "/api/player/previous"
+POST_PLAYER_PAUSE = "/api/player/pause"
+POST_PLAYER_PAUSE_TOGGLE = "/api/player/pause/toggle"
+POST_PLAYER_RANDOM = "/api/player/random"
+
+GET_PLAYLIST_ITEMS = "/api/playlists/{0}/items/{1}"
+GET_ALBUM_ART = "/api/artwork/{0}/{1}"
+
+# Playback modes
+PLAYBACK_MODE_DEFAULT = 0
+PLAYBACK_MODE_REPEAT_PLAYLIST = 1
+PLAYBACK_MODE_REPEAT_TRACK = 2
+PLAYBACK_MODE_RANDOM = 3
+PLAYBACK_MODE_SHUFFLE_TRACKS = 4
+PLAYBACK_MODE_SHUFFLE_ALBUMS = 5
+PLAYBACK_MODE_SHUFFLE_FOLDERS = 6
+
+
+class Foobar2k:
+    """Api access to Foobar 2000 Server"""
+
+    def __init__(self, host, port, timeout):
+
+        self._host = host
+        self._port = port
+        self._timeout = timeout
+        self._base_url = "http://{host}:{port}".format(
+            host=self._host, port=self._port)
+        _LOGGER.debug("[Foobar2k] __int__  with {0}".format(
+            self._base_url))
+
+        self._title = None
+        self._state = None
+        self._track_duration = 0
+        self._track_position = 0
+        self._isMuted = False
+        self._min_volume = -100
+        self._album_art_url = None
+        self._artist = None
+        self._album = None       
+
+    def send_get_command(self, command, data):
+        """Send command via HTTP get to FB2K server."""
+
+        res = requests.get("{base_url}{command}".format(
+            base_url=self._base_url, command=command), data=data,
+            timeout=self.timeout)
+        if (res.status_code == 200 or res.status_code == 204):
+            return res.text
+        else:
+            _LOGGER.error((
+                "Host %s returned HTTP status code %s to GET command at "
+                "end point %s"), self._host, res.status_code, command)
+            return None
+
+    def send_post_command(self, command, data):
+        """Send command via HTTP post to FB2K server."""
+
+        res = requests.post("{base_url}{command}".format(
+            base_url=self._base_url, command=command),
+            data=data, timeout=self.timeout)
+        if (res.status_code == 200 or res.status_code == 204):
+            return res.text
+        else:
+            _LOGGER.error((
+                "Host %s returned HTTP status code %s to POST command at "
+                "end point %s"), self._host, res.status_code, command)
+            return None
+
+    def update(self):
+        """
+        Get the latest status information from FB2K server
+        """
+
+        _LOGGER.debug("[Foobar2k] Doing update()")
+        # Get current status of the FB2K server
+        try:
+            response = self.send_get_command(GET_PLAYER_INFO, data=None)
+            self._power = POWER_ON
+            _LOGGER.debug("[Foobar2k] Doing update() POWER ON")
+        except ValueError:
+            pass
+        except requests.exceptions.RequestException:
+            # On timeout and connection error, the device is probably off
+            self._power = POWER_OFF
+            _LOGGER.debug("[Foobar2k] Doing update() POWER OFF")
+        else:
+            # Get current status
+            _LOGGER.debug("[Foobar2k] Doing update() Reading response")
+            if (response is not None):
+                data = json.loads(response)
+                _LOGGER.debug("[Foobar2k] Doing update() Loaded response {0}".format(data))
+
+                self._state = data["player"]["playbackState"]
+                self._playbackMode = data["player"]["playbackMode"]
+                if 'activeItem' in data["player"]:
+                    self._track_duration = data["player"]["activeItem"]["duration"]
+                    self._track_position = data["player"]["activeItem"]["position"]
+                    pl = data["player"]["activeItem"]["playlistId"]
+                    index = data["player"]["activeItem"]["index"]
+                    if (pl != "" and index != -1):
+                        self._album_art_url = "{0}{1}".format(self._base_url, GET_ALBUM_ART.format(pl, index))
+                        currently = self.send_get_command(GET_PLAYLIST_ITEMS.format(
+                            pl, index), data='{"columns":["%artist%","%title%", "%track%", "%album%"]}')
+                        if (currently is not None):
+                            _LOGGER.debug("[Foobar2k] Doing update() Have current song")
+                            i = json.loads(currently)
+                            self._artist = i["playlistItems"]["items"][0]["columns"][0]
+                            self._title = i["playlistItems"]["items"][0]["columns"][1]
+                            self._album = i["playlistItems"]["items"][0]["columns"][3]
+                            _LOGGER.debug("Currently Playing {0} {1}".format(self._artist, self._title))
+                    else:
+                        _LOGGER.info("No Song selected.")
+
+                if 'volume' in data["player"]:
+                    self._isMuted = data["player"]["volume"]["isMuted"]
+                    self._volume = data["player"]["volume"]["value"]
+                    self._min_volume = data["player"]["volume"]["min"]
+
+            # Finished
+            return True
+
+    @property
+    def host(self):
+        """Return the host ip as string."""
+        return self._host
+
+    @property
+    def port(self):
+        """Return the port as a number."""
+        return self._port
+
+    @property
+    def timeout(self):
+        """Return the timeout as a number."""
+        return self._timeout
+
+    @property
+    def isMuted(self):
+        """Is Muted True / False."""
+        return self._isMuted
+
+    @property
+    def isShuffle(self):
+        """Is Shuffle True / False."""
+        return self._playbackMode == PLAYBACK_MODE_RANDOM
+
+    @property
+    def volume(self):
+        """Volume level."""
+        return self._volume + abs(self._min_volume)
+
+    @property
+    def state(self):
+        """Can be paused, stopped, playing"""
+        _LOGGER.debug("[Foobar2k] Getting the state {0}".format(self._state))
+        return self._state
+
+    @property
+    def power(self):
+        """Can be on, off"""
+        return self._power
+
+    @property
+    def title(self):
+        """Song title"""
+        return self._title
+
+    @property
+    def album(self):
+        """Name of album"""
+        return self._album
+
+    @property
+    def album_art(self):
+        """ Album art work url"""
+        return self._album_art_url
+
+    @property
+    def artist(self):
+        """Name of artist"""
+        return self._artist
+
+    @property
+    def track_position(self):
+        """Playing position of the track"""
+        return self._track_position
+
+    @property
+    def track_duration(self):
+        """Duration of the Track"""
+        return self._track_duration
+
+    def toggle_play_pause(self):
+        """Toggle play pause media player."""
+        _LOGGER.debug("[Foobar2k] In Play / Pause")
+        if (self._power == POWER_ON):
+            _LOGGER.debug("[Foobar2k] Play / Pause Calling")
+            self.send_post_command(POST_PLAYER_PAUSE_TOGGLE, data=None)
+
+    def play(self):
+        """Send play command to FB2K Server"""
+        _LOGGER.debug("[Foobar2k] In Play")
+        if (self._power == POWER_ON):
+            response = self.send_post_command(POST_PLAYER_PLAY, data=None)
+            if (response is not None):
+                self._state = STATE_PLAYING
+
+    def play_next(self):
+        """Send stop command to FB2K Server"""
+        _LOGGER.debug("[Foobar2k] In Next")
+        if (self._power == POWER_ON):
+            self.send_post_command(POST_PLAYER_NEXT, data=None)
+            time.sleep(0.2)
+            self.update()
+
+    def play_previous(self):
+        """Send stop command to FB2K Server"""
+        _LOGGER.debug("[Foobar2k] In Previous")
+        if (self._power == POWER_ON):
+            self.send_post_command(POST_PLAYER_PREVIOUS, data=None)
+            time.sleep(0.2)
+            self.update()
+
+    def toggle_mute(self):
+        """Mute the volume."""
+        _LOGGER.debug("[Foobar2k] In Toggle Mute")
+        if (self._power == POWER_ON):
+            mute = not self._isMuted
+            data = json.dumps({"isMuted": mute})
+            _LOGGER.debug("[Foobar2k] Toggle data {0}".format(data))
+            self.send_post_command(POST_PLAYER, data=data)
+            self._isMuted = mute
+
+    def set_volume(self, volume):
+        """Change the volume."""
+        _LOGGER.debug("[Foobar2k] In Volume {0}".format(volume))
+        if (self._power == POWER_ON):
+            new_volume = self._min_volume + volume
+            data = json.dumps({"volume": new_volume})
+            _LOGGER.debug("[Foobar2k] Volume data {0}".format(data))
+            self.send_post_command(POST_PLAYER, data=data)
+            self._volume = new_volume
+
+    def set_position(self, position):
+        """Change the track playing position."""
+        _LOGGER.debug("[Foobar2k] In Position {0}".format(position))
+        if (self._power == POWER_ON):
+            data = json.dumps({"position": position})
+            _LOGGER.debug("[Foobar2k] Position data {0}".format(data))
+            self.send_post_command(POST_PLAYER, data=data)
+            self._track_position = position
+
+    def set_playbackMode(self, mode):
+        """Change the playback mode. Can be Default, Repeat (PlayList), Repeat (Track), Random, Shuffle (Tracks), Shuffle (Albums), Shuffle (Folders)"""
+        _LOGGER.debug("[Foobar2k] In Playback Mode {0}".format(mode))
+        if (self._power == POWER_ON):
+            data = json.dumps({"playbackMode": mode})
+            _LOGGER.debug("[Foobar2k] PlaybackMode data {0}".format(data))
+            self.send_post_command(POST_PLAYER, data=data)
+            self._playbackMode = mode
---
+++ foobar2k/media_player.py
@@ -0,0 +1,241 @@
+"""Support for Foobar2k api provided by beefweb https://github.com/hyperblast/beefweb."""
+
+from collections import namedtuple
+import logging
+from datetime import timedelta
+
+import voluptuous as vol
+
+from homeassistant.components.media_player import (
+    MediaPlayerDevice, PLATFORM_SCHEMA)
+
+# https://github.com/home-assistant/home-assistant/blob/dev/homeassistant/components/media_player/const.py
+from homeassistant.components.media_player.const import (
+    SUPPORT_TURN_ON, SUPPORT_TURN_OFF, SUPPORT_PLAY_MEDIA, SUPPORT_STOP, SUPPORT_PLAY, SUPPORT_SHUFFLE_SET, SUPPORT_SEEK,
+    SUPPORT_PAUSE, SUPPORT_VOLUME_MUTE, SUPPORT_VOLUME_STEP, SUPPORT_VOLUME_SET, SUPPORT_PREVIOUS_TRACK, SUPPORT_NEXT_TRACK,
+    MEDIA_TYPE_MUSIC, ATTR_APP_NAME, ATTR_MEDIA_ALBUM_ARTIST, ATTR_MEDIA_ALBUM_NAME, ATTR_MEDIA_DURATION, ATTR_MEDIA_PLAYLIST,
+    ATTR_MEDIA_SHUFFLE, ATTR_MEDIA_TITLE, ATTR_MEDIA_TRACK, ATTR_MEDIA_VOLUME_MUTED, )
+
+from homeassistant.const import (
+    CONF_HOST, CONF_NAME, CONF_PORT, CONF_TIMEOUT, STATE_OFF, STATE_ON,  STATE_PAUSED, STATE_PLAYING, STATE_UNKNOWN, STATE_IDLE)
+
+from custom_components.foobar2k.foobar2k import (
+    PLAYBACK_MODE_DEFAULT, PLAYBACK_MODE_REPEAT_PLAYLIST, PLAYBACK_MODE_REPEAT_TRACK, PLAYBACK_MODE_RANDOM,
+    PLAYBACK_MODE_SHUFFLE_TRACKS, PLAYBACK_MODE_SHUFFLE_ALBUMS, PLAYBACK_MODE_SHUFFLE_FOLDERS)
+
+import homeassistant.helpers.config_validation as cv
+import homeassistant.util.dt as dt_util
+
+_LOGGER = logging.getLogger(__name__)
+
+DEFAULT_TIMEOUT = 2
+
+SUPPORT_FOOBAR_PLAYER = \
+    SUPPORT_NEXT_TRACK | \
+    SUPPORT_PAUSE | \
+    SUPPORT_PLAY | \
+    SUPPORT_PREVIOUS_TRACK | \
+    SUPPORT_SHUFFLE_SET | \
+    SUPPORT_STOP | \
+    SUPPORT_VOLUME_MUTE |  \
+    SUPPORT_VOLUME_SET
+# SUPPORT_SEEK | \
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_HOST): cv.string,
+    vol.Optional(CONF_NAME): cv.string,
+    vol.Required(CONF_PORT): cv.port,
+    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
+})
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Foobar 2k platform."""
+    from custom_components.foobar2k.foobar2k import Foobar2k
+
+    name = config.get(CONF_NAME)
+    host = config.get(CONF_HOST)
+    port = config.get(CONF_PORT)
+    timeout = config.get(CONF_TIMEOUT)
+
+    fb2k_service = Foobar2k(
+        host=host, port=port, timeout=timeout)
+
+    add_entities([Foobar2kDevice(hass, fb2k_service, name)])
+
+
+class Foobar2kDevice(MediaPlayerDevice):
+
+    def __init__(self, hass, fb2k, name):
+        """Initialize the device."""
+        self.hass = hass
+        self._name = name
+        self._state = STATE_UNKNOWN
+        self._service = fb2k
+        self._title = None
+        self._artist = None
+        self._album = None
+        self._album_art = None
+        self._isMuted = False
+        self._volume = None
+        self._track_position = None
+        self._track_duration = None
+        self._shuffle = False
+        # self._last_update = None
+
+    def update(self):
+        self._service.update()
+        self._isMuted = self._service.isMuted
+        self._volume = self._service.volume
+        self._shuffle = self._service.isShuffle
+        if (self.state == STATE_PLAYING):
+            self._album_art = self._service.album_art
+            self._title = self._service.title
+            self._artist = self._service.artist
+            self._album = self._service.album
+            self._track_position = self._service.track_position
+            self._track_duration = self._service.track_duration
+            # self._last_update = dt_util.utcnow()
+
+    @property
+    def name(self):
+        """Return the name of the device."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the device."""
+        current_state = self._service.state
+        if (current_state == "playing"):
+            self._state = STATE_PLAYING
+        elif (current_state == "paused"):
+            self._state = STATE_PAUSED
+        else:
+            self._state = STATE_IDLE
+        _LOGGER.debug("Current State {0}".format(self._state))
+
+        return self._state
+
+    @property
+    def supported_features(self):
+        """Flag media player features that are supported."""
+        supported_features = SUPPORT_FOOBAR_PLAYER
+        return supported_features
+
+    @property
+    def shuffle(self):
+        """Boolean if shuffling is enabled."""
+        return self._shuffle
+
+    @property
+    def is_volume_muted(self):
+        return self._isMuted
+
+    @property
+    def volume_level(self):
+        """Volume level of the media player (0 to 1)."""
+        if (self._volume == 0 or self._volume == None):
+            return 0
+        return float(self._volume) / 100  
+
+    @property
+    def media_title(self):
+        """Title of current playing track."""
+        return self._title
+
+    @property
+    def media_content_type(self):
+        """Content type of current playing media."""
+        return MEDIA_TYPE_MUSIC
+
+    @property
+    def media_artist(self):
+        """Artist of current playing track."""
+        return self._artist
+
+    @property
+    def media_album_name(self):
+        """Album name of current playing track."""
+        return self._album
+
+    @property
+    def media_duration(self):
+        """Return the duration of current playing media in seconds."""
+        _LOGGER.debug("[Media_Player_FB2K] media_duration Called {0}".format(
+            self._track_duration))
+        return self._track_duration
+
+    @property
+    def media_position(self):
+        """Return the position of current playing media in seconds."""
+        _LOGGER.debug("[Media_Player_FB2K] media_position Called {0}".format(
+            self._track_position))
+        return self._track_position
+
+    # @property
+    # def media_position_updated_at(self):
+    #     """When was the position of the current playing media valid."""
+    #     return self._last_update
+
+    @property
+    def media_image_url(self):
+        """Image url of current playing media."""
+        _LOGGER.debug("[Media_Player_FB2K] Album ART")
+        return self._album_art
+
+    # @property
+    # def should_poll(self):
+    #     """Return True if entity has to be polled for state."""
+    #     return True
+
+    def media_play_pause(self):
+        """Send the media player the command for play/pause."""
+        _LOGGER.debug("[Media_Player_FB2K] Play / Pause Called")
+        self._service.toggle_play_pause()
+
+    def media_pause(self):
+        """Send the media player the command for play/pause if playing."""
+        _LOGGER.debug("[Media_Player_FB2K] Pause Called")
+        if (self.state == STATE_PLAYING):
+            _LOGGER.debug("[Media_Player_FB2K] Pausing")
+            self.media_play_pause()
+
+    def media_stop(self):
+        """Send the media player the stop command."""
+        _LOGGER.debug("[Media_Player_FB2K] Stop Called")
+        self._service.stop()
+
+    def media_play(self):
+        """Send the media player the command to play at the current playlist."""
+        _LOGGER.debug("[Media_Player_FB2K] Play Called")
+        self._service.play()
+
+    def media_next_track(self):
+        """Send the media player the command to play the next song"""
+        _LOGGER.debug("[Media_Player_FB2K] Next Track Called")
+        self._service.play_next()
+
+    def media_previous_track(self):
+        """Send the media player the command to play the previous song"""
+        _LOGGER.debug("[Media_Player_FB2K] Next Track Called")
+        self._service.play_previous()
+
+    def mute_volume(self, mute):
+        """Mute the volume."""
+        _LOGGER.debug("[Media_Player_FB2K] Mute Called")
+        self._service.toggle_mute()
+
+    def set_volume_level(self, volume):
+        """Send the media player the command for setting the volume."""
+        _LOGGER.debug("[Media_Player_FB2K] set_volume_level Called {0}".format(volume))
+        self._service.set_volume(volume * 100)
+
+    # def media_seek(self, position):
+    #     """Send the media player a command for seeking new position in track."""
+    #     self._service.set_position(position)
+
+    def set_shuffle(self, shuffle):
+        """Send the media player the command for setting the shuffle mode."""
+        _LOGGER.debug("[Media_Player_FB2K] set_shuffle Called **{0}**".format(shuffle))
+        mode = PLAYBACK_MODE_RANDOM if shuffle else PLAYBACK_MODE_DEFAULT
+        self._service.set_playbackMode(mode)
